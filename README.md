84b6de0696c052f019821174f2997c35e509bd7b
Hung Nguyen <hung.nguyen.francis@proton.me>
2025-06-05 22:38:21 +0700

    reup cache-cleanup repo with some improvements

---
+++ README.md
@@ -0,0 +1,14 @@
+# Cache Cleanup Script
+A simple Arch Linux maintenance script to clean package cache, remove orphaned packages & configs, and clear user’s ~/.cache.
+
+## Usage:
+Please run the following commands to download and run the script:
+```bash
+git clone https://github.com/Arch-Linux-notes/Arch-Cache-Cleanup-Script.git
+cd Arch-Cache-Cleanup-Script
+sudo chmod +x cache-cleanup.sh
+./cache-cleanup.sh
+```
+
+## Notes:
+- This script works on the Arch Linux operating system.
---
+++ cache-cleanup.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# clear the screen
+clear
+
+# avoid some implicit errors
+set -euo pipefail
+
+# title of the script
+echo '
+----------------------------------------------------------------
+       ░█▀▀░█▀█░█▀▀░█░█░█▀▀░░░█▀▀░█░░░█▀▀░█▀█░█▀█░█░█░█▀█
+       ░█░░░█▀█░█░░░█▀█░█▀▀░░░█░░░█░░░█▀▀░█▀█░█░█░█░█░█▀▀
+       ░▀▀▀░▀░▀░▀▀▀░▀░▀░▀▀▀░░░▀▀▀░▀▀▀░▀▀▀░▀░▀░▀░▀░▀▀▀░▀░░
+----------------------------------------------------------------
+'
+
+# clean pacman cache
+echo '==> Cleaning pacman cache...'
+sudo pacman -Sc
+sudo pacman -Scc
+echo '
+----------------------------------------------------------------
+'
+
+# find and remove orphan packages
+pkglist=$(pacman -Qdtq 2>/dev/null || echo '')
+orphans=$(echo "$pkglist" | tr '\n' ' ' | xargs)
+if [ -n "$orphans" ]; then
+    echo 'Orphan packages detected:'
+    echo "$orphans"
+    echo '==> Cleaning orphan packages...'
+    sudo pacman -Rcuns "$orphans"
+else
+    echo 'No orphan packages found.'
+fi
+echo '
+----------------------------------------------------------------
+'
+
+# clean .cache folder in HOME directory
+CACHE_DIR="$HOME/.cache"
+echo 'Cache size before cleanup:'
+du -sh "$CACHE_DIR"
+echo "==> Removing all files in $CACHE_DIR..."
+rm -rf "$CACHE_DIR"/*
+echo 'Cache size after cleanup:'
+du -sh "$CACHE_DIR"
+echo '
+----------------------------------------------------------------
+'
+
+# remove all .bash_history*.tmp files in HOME
+tmplist=$(ls "$HOME"/.bash_history*.tmp 2>/dev/null || echo '')
+temps=$(echo "$tmplist" | tr '\n' ' ' | xargs)
+if [ -n "$temps" ]; then
+    echo 'Temporary bash history files detected:'
+    echo "$temps"
+    echo '==> Removing temporary bash history files...'
+    rm "$temps"
+else
+    echo 'No temporary bash history files found.'
+fi
+echo '
+----------------------------------------------------------------
+'
+
+# script completed
+echo 'Cleanup complete!'
